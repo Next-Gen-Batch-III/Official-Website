0258c99585983d0d9fbcbbdcb9c04daa60668e14
NgetSokunkanha <ngetsokunkanha1@gmail.com>
2026-05-17 16:28:59 +0700

    Overview page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
+import Overview from './pages/Overview';
 import Footer from './components/layout/footer/Footer';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/overview" element={<Overview />} />
         </Routes>
         <Footer />
       </BrowserRouter>
